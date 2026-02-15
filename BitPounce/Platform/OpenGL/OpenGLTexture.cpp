838b5ef7ff8fdaa055e835d98d4d4f7bce828a33
Cats Studio <79156677+iojuedioe9rd@users.noreply.github.com>
2026-02-15 20:45:04 +0000

    added OpenAL-Soft

--- BitPounce/Platform/OpenGL/OpenGLTexture.cpp
+++ BitPounce/Platform/OpenGL/OpenGLTexture.cpp
@@ -8,6 +8,21 @@
 
 namespace BitPounce {
 
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+		: m_Width(width), m_Height(height)
+	{
+		m_DataFormat = GL_RGBA;
+
+		glGenTextures(1, &m_RendererID);
+		glBindTexture(GL_TEXTURE_2D, m_RendererID);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
+
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
 		: m_Path(path)
 	{
@@ -38,6 +53,8 @@ namespace BitPounce {
 		else if (channels == 3) format = GL_RGB;
 		else if (channels == 4) format = GL_RGBA;
 
+		m_DataFormat = format;
+
 		glTexImage2D(GL_TEXTURE_2D, 0, format, m_Width, m_Height, 0, format, GL_UNSIGNED_BYTE, imageData);
 
 		stbi_image_free(imageData);
@@ -46,6 +63,13 @@ namespace BitPounce {
 	OpenGLTexture2D::~OpenGLTexture2D()
 	{
 		glDeleteTextures(1, &m_RendererID);
+	}
+
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+		BP_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
+		glTexImage2D(GL_TEXTURE_2D, 0, m_DataFormat, m_Width, m_Height, 0, m_DataFormat, GL_UNSIGNED_BYTE, data);
 	}
 
 	void OpenGLTexture2D::Bind(uint32_t slot) const
